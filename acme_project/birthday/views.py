57a3ae9f8eece50dbdbc1a06a33a6e976790f705
Богданов Артём <bogdartem1997@yandex.ru>
2023-06-03 02:33:54 +0400

    Add birthdays redacting

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .forms import BirthdayForm
+from .models import Birthday
 # Импортируем из utils.py функцию для подсчёта дней.
 from .utils import calculate_birthday_countdown
+
+
+def edit_birthday(request, pk):
+    # Находим запрошенный объект для редактирования по первичному ключу
+    # или возвращаем 404 ошибку, если такого объекта нет.
+    instance = get_object_or_404(Birthday, pk=pk)
+    # Связываем форму с найденным объектом: передаём его в аргумент instance.
+    form = BirthdayForm(request.POST or None, instance=instance)
+    # Всё остальное без изменений.
+    context = {'form': form}
+    # Сохраняем данные, полученные из формы, и отправляем ответ:
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            form.cleaned_data['birthday']
+        )
+        context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
 
 
 def birthday(request):
@@ -20,3 +39,11 @@ def birthday(request):
         # Обновляем словарь контекста: добавляем в него новый элемент.
         context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context)
